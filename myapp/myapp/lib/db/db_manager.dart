6c3a665ec6fa7c5cd3ec4898a1ca02a591478c1f
136759790 <136759790@qq.com>
2020-09-25 17:35:56 +0800

    1111

--- myapp/myapp/lib/db/db_manager.dart
+++ myapp/myapp/lib/db/db_manager.dart
@@ -1,3 +1,53 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DBManager {
   static const DBNAME = "xiaodian";
+  static Database _db;
+
+  static init() async {
+    var dbPath = await getDatabasesPath();
+    String path = dbPath + DBNAME;
+    if (Platform.isIOS) {
+      path = dbPath + "/" + DBNAME;
+    }
+    _db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        String sql_icon = await rootBundle.loadString('assets/sql/icon.sql');
+        await db.execute(sql_icon);
+        String sql_user_icon =
+            await rootBundle.loadString('assets/sql/user_icon.sql');
+        await db.execute(sql_user_icon);
+        String sql_icon_data =
+            await rootBundle.loadString('assets/sql/icon_data.sql');
+        await db.execute(sql_icon_data);
+        String sql_user_icon_data =
+            await rootBundle.loadString('assets/sql/user_icon_data.sql');
+        await db.execute(sql_user_icon_data);
+      },
+    );
+  }
+
+  static Future<Database> getDb() async {
+    if (_db == null) {
+      await init();
+    }
+    return _db;
+  }
+
+  static close() {
+    _db?.close();
+    _db = null;
+  }
+
+  static isTableExits(name) async {
+    await getDb();
+    var res = await _db.rawQuery(
+        "select * from Sqlite_master where type = 'table' and name = '$name'");
+    return res != null && res.length > 0;
+  }
 }
---
+++ myapp/myapp/lib/icons/iconfonts.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class IconFont {
+  static const IconData basketball_hool =
+      IconData(0xe60c, fontFamily: 'IconFonts');
+}
